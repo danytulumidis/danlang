cdc3a7333c3f525e8a3c465349647b0f22fbf865
danytulumidis <dextavision@gmail.com>
2024-07-24 21:38:03 +0200

    adding functions handling the creation of ast nodes and memory allocation

--- ast.c
+++ ast.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum {
     AST_NUMBER,
     AST_VARIABLE,
     AST_BINARY,
-    AST_CALL_EXPR,
+    AST_FUNCTION_CALL,
     AST_PROTOTYPE,
     AST_FUNCTION
 } ASTNodeType;
@@ -27,7 +28,7 @@ typedef struct {
 // Variable AST node
 typedef struct {
     ASTNode base;
-    double *name;
+    char *name;
 } VariableAST;
 
 // Binary expression AST node
@@ -75,5 +76,77 @@ NumberAST *createNumberAST(double value) {
 
     node->base.type = AST_NUMBER;
     node->value = value;
+    return node;
+}
+
+VariableAST *createVariableAST(const char *name) {
+    VariableAST *node = (VariableAST *)malloc(sizeof(VariableAST));
+
+    if (!node) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(1);
+    }
+
+    node->base.type = AST_VARIABLE;
+    node->name = strdup(name);
+    return node;
+}
+
+BinaryAST *createBinaryAST(char op, ASTNode *lhs, ASTNode *rhs) {
+    BinaryAST *node = (BinaryAST *)malloc(sizeof(BinaryAST));
+
+    if (!node) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(1);
+    }
+
+    node->base.type = AST_BINARY;
+    node->op = op;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+FunctionCallAST *createFunctionCallAST(const char *callee, ASTNode **args, int num_args) {
+    FunctionCallAST *node = (FunctionCallAST *)malloc(sizeof(FunctionCallAST));
+
+    if (!node) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(1);
+    }
+
+    node->base.type = AST_FUNCTION_CALL;
+    node->callee = strdup(callee);
+    node->args = args;
+    node->num_args = num_args;
+    return node;
+}
+
+PrototypeAST *createPrototypeAST(const char *name, char **args, int num_args) {
+    PrototypeAST *node = (PrototypeAST *)malloc(sizeof(PrototypeAST));
+
+    if (!node) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(1);
+    }
+
+    node->base.type = AST_PROTOTYPE;
+    node->name = strdup(name);
+    node->args = args;
+    node->num_args = num_args;
+    return node;
+}
+
+FunctionAST *createFunction(PrototypeAST *proto, ASTNode *body) {
+    FunctionAST *node = (FunctionAST *)malloc(sizeof(FunctionAST));
+
+    if (!node) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(1);
+    }
+
+    node->base.type = AST_FUNCTION;
+    node->proto = proto;
+    node->body = body;
     return node;
 }
